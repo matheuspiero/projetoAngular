06fd04eeb4d4eb4f3217498745e3693bcc34c17c
Matheus Sena <matheuspiero19@gmail.com>
2024-08-08 16:31:50 -0300

    Projeto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
+import { PerfilContatoComponent } from './paginas/perfil-contato/perfil-contato.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    FormularioContatoComponent,
+    ListaContatosComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'indexa';
+  
 }
